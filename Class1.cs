f2ad7cb10ee2f959eb53ac97696100a94a428ada
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-27 16:12:26 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Kata
+{
+    public static String Problem(String a)
+    {
+        try
+        {
+            return (Convert.ToDouble(a) * 50 + 6).ToString();
+        }
+        catch
+        {
+            return "Error";
+        }
+    }
+}
